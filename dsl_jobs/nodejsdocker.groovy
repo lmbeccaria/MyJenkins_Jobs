1ecc69fcd54d9eb8725c8ec3fdcc660314733e11
Mariano <lmbeccaria@gmail.com>
2020-01-23 18:42:35 -0700

    First Commit - ds_jobs dir

---
+++ dsl_jobs/nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS Docker example') {
+    scm {
+        git('git://github.com/lmbeccaria/docker-nodejs-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('vinoveritas40@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('wardviaene/docker-nodejs-demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
